d927bbb10afd1beb98845d1d4a4f70ae17d635ad
khwolfe <kenneth.wolfe@ucd.ie>
2014-04-25 00:08:45 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -349,7 +349,7 @@
 
 					$('#feed-' + data.id + ' .duration-90').click(function() {
 						$('#loadingData').foundation('reveal', 'open');
-						updateFeeds(data.id, thisFeedDatastreams, '1year', 10800);
+						updateFeeds(data.id, thisFeedDatastreams, '1year', 86400);
 						return false;
 					});
 
