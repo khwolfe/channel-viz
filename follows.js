5794684dd7e1d568a8698002d56f623d2335959c
khwolfe <kenneth.wolfe@ucd.ie>
2014-04-24 23:47:40 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,7 +16,7 @@
 		defaultFeeds	= [1282406454], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Kenzapp', // Replaces Xively logo in the header
 		dataDuration	= '1week', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
